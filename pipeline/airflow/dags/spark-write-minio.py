06a46eb41645fd615647d35cbb4455d74d2c98e1
lam1051999 <lam1051999.com>
2025-02-12 12:03:04 +0700

    Add Airflow installation

---
+++ pipeline/airflow/dags/spark-write-minio.py
@@ -0,0 +1,19 @@
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+
+with DAG(
+    dag_id="spark-write-minio",
+    schedule=None,
+    start_date=days_ago(2),
+    catchup=False,
+    dagrun_timeout=pendulum.duration(minutes=1),
+    tags=["spark-write-minio"],
+) as dag:
+    spark_job = SparkKubernetesOperator(
+        task_id='spark-job',
+        application_file="/opt/airflow/dags/repo/pipeline/airflow/spark/spark-write-minio.yaml",
+        namespace="data-platform"
+    )
