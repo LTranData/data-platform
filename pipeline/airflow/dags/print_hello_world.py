bfdabf5073bc35692ab4ff3d5ac4e26ab8608ca3
lam1051999 <lam1051999.com>
2025-02-12 10:55:23 +0700

    Add Airflow installation

--- pipeline/airflow/dags/print_hello_world.py
+++ pipeline/airflow/dags/print_hello_world.py
@@ -1,24 +1,23 @@
-from __future__ import annotations
-
 import pendulum
 
 from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
+from datetime import timedelta
 
 def print_hello():
     print("Hello, World!")
 
 with DAG(
     dag_id="print_every_5_seconds",
-    schedule="@every_5s",  # Run every 5 seconds
+    schedule=timedelta(seconds=5),  # Use timedelta for 5-second interval
     start_date=days_ago(2),
     catchup=False,
-    dagrun_timeout=pendulum.duration(minutes=1), # Timeout for the entire DAG run
+    dagrun_timeout=pendulum.duration(minutes=1),
     tags=["example"],
 ) as dag:
     print_hello_task = PythonOperator(
         task_id="print_hello",
         python_callable=print_hello,
-        provide_context=True, 
+        provide_context=True,
     )
